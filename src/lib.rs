23ab1a8649b7c8326d23cdf3f2d61fa6d101e341
RyanCDE <111553399+RyanCDE@users.noreply.github.com>
2025-10-02 13:01:40 +1000

    Organize CAN drivers under drivers module

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,5 @@
-/// Exposes a serial port as a CAN interface.
-pub mod slcan;
-pub use slcan::SlcanDriver;
-
-pub mod can_driver;
-pub mod gs_usb;
-pub mod pcan;
-pub use can_driver::CanDriver;
-pub use gs_usb::GsUsbDriver;
-pub use pcan::PcanDriver;
+/// Collection of supported CAN drivers.
+pub mod drivers;
+pub use drivers::{CanDriver, GsUsbDriver, PcanDriver, SlcanDriver};
 /// We'll create this instead of thread_manager.rs
 pub mod thread_manager_async;
